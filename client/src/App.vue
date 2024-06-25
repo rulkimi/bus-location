26570f888dc7b13be2bfb6ad0d5edfff733ca161
rulkimi <azrulkimi00@gmail.com>
2024-06-25 23:15:29 +0800

    click to search the route

--- client/src/App.vue
+++ client/src/App.vue
@@ -3,13 +3,13 @@
     <div class="container mx-auto px-4 py-8">
       <!-- Available Routes List -->
       <div class="mt-4">
-        <p class="text-lg font-semibold mb-2">Available Routes:</p>
+        <h1 class="text-2xl font-bold mb-4">Available Routes:</h1>
         <div class="flex flex-wrap">
           <div
             v-for="route in routes"
             :key="route"
             class="bg-white shadow-md rounded-lg mb-2 px-4 py-2 cursor-pointer hover:bg-gray-100 mr-2"
-            @click="selectRoute(route)"
+            @click="fetchLocation(route)"
             style="max-width: calc(100% - 4px);"
           >
             {{ route }}
@@ -17,34 +17,36 @@
         </div>
       </div>
 
-      <div class="grid grid-cols-1 md:grid-cols-3 gap-4">
-        <!-- Left side (1/3 width on medium screens and larger) -->
-        <div class="md:col-span-1">
-          <div class="bg-white p-6 rounded-lg shadow-md">
-            <h1 class="text-2xl font-bold mb-4">Bus Locations Map</h1>
-            <input
-              type="text"
-              v-model="routeId"
-              class="route-input border border-gray-300 px-3 py-2 rounded-md w-full mb-4"
-              placeholder="Enter route ID"
-            />
-            <button
-              @click="fetchLocation"
-              class="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-md w-full"
-            >
-              Search
-            </button>
-
-            <bus-list :buses="buses" @busSelected="setMapView" class="mt-4" />
+      <div class="mt-4">
+        <h1 class="text-2xl font-bold mb-4">Bus Location</h1>
+        <div class="grid grid-cols-1 md:grid-cols-3 gap-4">
+          <!-- Left side (1/3 width on medium screens and larger) -->
+          <div class="md:col-span-1">
+            <div class="bg-white p-6 rounded-lg shadow-md">
+              <input
+                type="text"
+                v-model="routeId"
+                class="route-input border border-gray-300 px-3 py-2 rounded-md w-full mb-4"
+                placeholder="Search route ID here or click from the list above"
+              />
+              <button
+                @click="fetchLocation(routeId)"
+                class="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-md w-full"
+              >
+                Search
+              </button>
+              <bus-list :buses="buses" @busSelected="setMapView" class="mt-4" />
+            </div>
           </div>
-        </div>
-        <!-- Right side (2/3 width on medium screens and larger) -->
-        <div class="md:col-span-2">
-          <div class="bg-white p-6 rounded-lg shadow-md">
-            <bus-map :buses="buses" ref="busMap" class="h-96" />
+          <!-- Right side (2/3 width on medium screens and larger) -->
+          <div class="md:col-span-2">
+            <div class="bg-white p-6 rounded-lg shadow-md">
+              <bus-map :buses="buses" ref="busMap" class="h-96" />
+            </div>
           </div>
         </div>
       </div>
+
     </div>
   </div>
 </template>
@@ -61,7 +63,7 @@ export default {
   },
   data() {
     return {
-      routeId: 'T8150',
+      routeId: '',
       buses: [],
       routes: []  // Array to store available routes
     };
@@ -70,9 +72,10 @@ export default {
     await this.fetchAllRoutes();
   },
   methods: {
-    async fetchLocation() {
+    async fetchLocation(routeId) {
+      this.routeId = routeId;
       try {
-        const response = await fetch(`http://localhost:8000/vehicle/${this.routeId}`);
+        const response = await fetch(`http://localhost:8000/vehicle/${routeId}`);
         const responseData = await response.json();
         this.buses = responseData.vehicles;
       } catch (error) {
